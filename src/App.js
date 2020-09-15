d91db93a6f68dfc753efaeede68d4e57a0a3b730
Jun Young Cho <godguman@gmail.com>
2020-09-16 01:35:01 +0700

    rspFunction

--- src/App.js
+++ src/App.js
@@ -1,20 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Box from "./component/Box";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const choises = {
-	rock: "https://pngimg.com/uploads/stone/stone_PNG13596.png",
-	paper:
-		"https://www.pinclipart.com/picdir/big/23-237847_notebook-paper-torn-png-picture-peace-be-upon.png",
-	scissor: "https://www.pngmart.com/files/1/Scissors-Realistic-PNG.png",
+const choices = {
+	rock: "img/rock.png",
+	paper: "img/paper.png",
+	scissor: "img/scissor.png",
+	questionMark: "img/question.png",
 };
 
 function App() {
+	//setItem()
+	let [playResult, setPlayResult] = useState("");
+	let [compResult, setCompResult] = useState("");
+	let [finalResult, setFinalResult] = useState("Start!");
+	let [item, setItem] = useState("questionMark");
+	let [computerItem, setComputerItem] = useState("questionMark");
+	let [gameHistory, setGameHistory] = useState([]);
+	//console.log(item, setItem);
+
+	const selectItem = (clickeditem) => {
+		let rsp = Object.keys(choices);
+		let currentPlayerChoice = clickeditem;
+		let currentComputerChoice = rsp[Math.floor(Math.random() * 3)];
+		setItem(currentPlayerChoice);
+		setComputerItem(currentComputerChoice);
+		if (currentPlayerChoice === currentComputerChoice) {
+			setPlayResult("tie");
+			setCompResult("tie");
+			setFinalResult("Draw");
+			gameHistory.push(finalResult);
+			setGameHistory(gameHistory);
+		}
+		if (currentPlayerChoice === "rock") {
+			if (currentComputerChoice === "paper") {
+				setPlayResult("lose");
+				setCompResult("win");
+				setFinalResult("Defeated");
+				gameHistory.push(finalResult);
+				setGameHistory(gameHistory);
+			} else if (currentComputerChoice === "scissor") {
+				setPlayResult("win");
+				setCompResult("lose");
+				setFinalResult("Victory");
+				gameHistory.push(finalResult);
+				setGameHistory(gameHistory);
+			}
+		}
+		if (currentPlayerChoice === "scissor") {
+			if (currentComputerChoice === "rock") {
+				setPlayResult("lose");
+				setCompResult("win");
+				setFinalResult("Defeated");
+				gameHistory.push(finalResult);
+				setGameHistory(gameHistory);
+			} else if (currentComputerChoice === "paper") {
+				setPlayResult("win");
+				setCompResult("lose");
+				setFinalResult("Victory");
+				gameHistory.push(finalResult);
+				setGameHistory(gameHistory);
+			}
+		}
+		if (currentPlayerChoice === "paper") {
+			if (currentComputerChoice === "scissor") {
+				setPlayResult("lose");
+				setCompResult("win");
+				setFinalResult("Defeated");
+				gameHistory.push(finalResult);
+				setGameHistory(gameHistory);
+			} else if (currentComputerChoice === "rock") {
+				setPlayResult("win");
+				setCompResult("lose");
+				setFinalResult("Victory");
+				gameHistory.push(finalResult);
+				setGameHistory(gameHistory);
+			}
+		}
+	};
 	return (
 		<div className="App">
-			<Box who="you" winner={true} imgURL={choises.rock} />
-			<Box who="computer" winner={false} imgURL={choises.paper} />
+			<div className="boxes">
+				<Box who="you" result={playResult} img={choices[item]} item={item} />
+				<Box
+					who="computer"
+					result={compResult}
+					img={choices[computerItem]}
+					item={computerItem}
+				/>
+			</div>
+			<div className="resLocation">
+				<div className="resHistory">
+					<h3>History</h3>
+					<ul>
+						{gameHistory.map((finalResult) => {
+							return <li>{finalResult}</li>;
+						})}
+					</ul>
+				</div>
+			</div>
+			<div>
+				<h1 className="finalRes">{finalResult}</h1>
+			</div>
+			<div className="button-group">
+				<button onClick={() => selectItem("rock")}>rock</button>
+				<button onClick={() => selectItem("scissor")}>scissor</button>
+				<button onClick={() => selectItem("paper")}>paper</button>{" "}
+			</div>
 		</div>
 	);
 }
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,26 +1,26 @@
-// import React from "react";
+import React from "react";
 
-// export default function Box({ winner, who, imgURL }) {
-// 	//let { num, value } = props;
-// 	return (
-// 		<div className={`Box ${winner ? "winner" : "loser"}`}>
-// 			<h1>{who}</h1>
-// 			<img src={imgURL} />
-// 			<h3>{winner ? "Won" : "Loss"}</h3>
-// 		</div>
-// 	);
-// }
-
-import React, { Component } from "react";
-
-export default class Box extends Component {
-	render() {
-		return (
-			<div className={`Box ${this.props.winner ? "winner" : "loser"}`}>
-				<h1>{this.props.who}</h1>
-				<img src={this.props.imgURL} />
-				<h3>{this.props.winner ? "Won" : "Loss"}</h3>
-			</div>
-		);
-	}
+export default function Box({ result, who, img, item }) {
+	//let { num, value } = props;
+	return (
+		<div className={`Box ${result} `}>
+			<h1>{who}</h1>
+			<img src={img} /> {item}
+			<h3>{result}</h3>
+		</div>
+	);
 }
+
+// import React, { Component } from "react";
+
+// export default class Box extends Component {
+// 	render() {
+// 		return (
+// 			<div className={`Box ${this.props.winner ? "winner" : "loser"}`}>
+// 				<h1>{this.props.who}</h1>
+// 				<img src={this.props.imgURL} />
+// 				<h3>{this.props.winner ? "Won" : "Loss"}</h3>
+// 			</div>
+// 		);
+// 	}
+// }
